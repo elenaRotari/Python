f03449bac9103a7c0a1bf861a290aca152a5ed60
Elena Rotari <elena.rotari@icloud.com>
2023-06-22 15:22:14 +0200

    first steps

---
+++ main.py
@@ -0,0 +1,11 @@
+
+#print(" Hello World!!")
+#input("Hi there, how are you?")
+#print("hello " + input("How is your name?"))
+#print("") → comment with #
+
+#lungimea string 
+string =input("whats your name ? ")
+print(len(string))
+#shortly
+print(len(input("What is your name? ")))
