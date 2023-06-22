6b97df51db3ca7fe6d508eda6ce3bcaed40d69ad
Elena Rotari <elena.rotari@icloud.com>
2023-06-22 16:50:47 +0200

    complet first day of Python bootcamp

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-
+#1 Exercise day 1 
 #print(" Hello World!!")
 #input("Hi there, how are you?")
+
+
+#2 Exercise day 1 
 #print("hello " + input("How is your name?"))
 #print("") → comment with #
 
-#lungimea string 
-string =input("whats your name ? ")
-print(len(string))
+#3 Exercize - day1
+#length of  string 
+#string =input("whats your name ? ")
+#print(len(string))
 #shortly
-print(len(input("What is your name? ")))
+#print(len(input("What is your name? ")))
+
+
+#4 Exercise day1 
+
+""""a= input("a: ")
+b= input("b: ")
+# we create a new variable c equap to a input
+c=a
+# we declare that there a is equal b 
+a=b
+
+b=c
+
+
+
+
+
+print("a: " + a)
+print("b: " + b)"""
+
+# Final Exerzice day1 #
+
+print("Welcome to the Band Name Generator")
+city= input("What's name of the city you grew up in? ")
+print(city)
+pet = input("What's your pet's name? ")
+print(pet)
+print('Your band name could be '+ city +" " +  pet )
+
+
