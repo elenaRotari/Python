9b94baad5d4a96a30159014f5c2e389c44c7ff2c
Elena Rotari <elena.rotari@icloud.com>
2023-06-27 15:09:01 +0200

    Leap year exercise done

--- main.py
+++ main.py
@@ -127,6 +127,26 @@ elif bmi>30 and bmi<35:
 else:
     print(f"Your BMI is {bmi}, you are clinically obese.")"""
 
+#Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February. The reason why we have leap years is really fascinating, this video does it more justice:
+"""year = int(input("Which year do you want to check? "))
+if year % 4==0 :
+ if year%100==0:
+  if year %400==0:
+    print("Leap year")
+  else:
+   print("Not leap year")
+ else:
+  print("Leap year")
+else:
+ print("Not leap year")"""
+
+
+
+
+
+
+
+
 
 
 
