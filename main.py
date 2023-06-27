a6daf6070c5570b97239e6963e5dc1494ea5c140
Elena Rotari <elena.rotari@icloud.com>
2023-06-27 14:47:46 +0200

    day3 BMI exercise with conditional operators done

--- main.py
+++ main.py
@@ -109,5 +109,24 @@ people = int(input("How many people to split the bill?"))
 final_amount = round(( bill + (bill * (tip / 100))) / people, 2)
 print(f"Each person should pay: ${final_amount}")"""
 
+# DAY3 Conditional Operators
+
+#Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+"""height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+bmi= round(int(weight) / (float(height) * float(height)))
+print(bmi)
+if bmi< 18.5:
+    print(f"Your BMI is {bmi}, you are underweight")
+elif bmi>=18.5 and bmi<25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi>=25 and bmi<30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi>30 and bmi<35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")"""
+
+
 
 
